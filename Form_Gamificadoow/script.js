9281a79cc1311c65ce1826b81d11be0d96ba0fbe
Bruna Sanches <110208238+brusanchesz@users.noreply.github.com>
2023-10-04 21:18:04 -0300

    Add files via upload

--- Form_Gamificadoow/script.js
+++ Form_Gamificadoow/script.js
@@ -8,23 +8,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Inicializa o progresso com 0%
     let progressValue = 0;
+    avatar.src = "img/triste.png";
 
     form.addEventListener("input", function () {
         const completedFields = form.querySelectorAll("input[required]:valid, select[required]:valid").length;
 
         // Calcula o progresso com base no número de campos preenchidos corretamente
-        progressValue = (completedFields / totalFields) * 100;
+        progressValue = (completedFields / totalFields) * 25;
         progressBar.style.width = progressValue + "%";
 
         // Lógica para mudar a expressão do avatar com base no progresso
-        if (progressValue > 0) {
+        if (progressValue > 0 && progressValue < 100) {
             avatar.src = "img/pensativa.png"; // Altere para a imagem neutra
-        } else {
-            avatar.src = "img/triste.png"; // Altere para a imagem triste
-        }
+        } 
     });
     
-
     
     form.addEventListener("submit", function (e) {
         e.preventDefault();
@@ -45,5 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
             // Caso algum campo esteja inválido, você pode adicionar um feedback ao usuário aqui
             alert("Por favor, preencha todos os campos obrigatórios corretamente.");
         }
+
     });
 });
